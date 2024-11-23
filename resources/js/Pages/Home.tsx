b5350cd4a0f85ca5958d90e85adfb533f46c1503
Manot22 <arimanut41@gmail.com>
2024-11-23 14:24:41 +0700

    change in homepage

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -4,6 +4,7 @@ const Home = () => {
             <div className="flex justify-center items-center">
                 <h1 className="font-bold text-2xl">Home Page</h1>
             </div>
+            <p>Helo Dunia</p>
         </main>
     );
 };
